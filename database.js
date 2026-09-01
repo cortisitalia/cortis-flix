643095cc968c5f1d51fba014133ac507ac680ef0
cortisitalia <cortisitalia@gmail.com>
2026-09-02 00:20:48 +0200

    database.js

--- database.js
+++ database.js
@@ -4,7 +4,8 @@ const videoData = [
     title: "Documentario Ep. 1",
     category: "documentario",
     videoSrc: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-    cover: "https://picsum.photos/400/225?random=1"
+    cover: "https://picsum.photos/400/225?random=1",
+    date: "2026-09-01" // Aggiungi la data se vuoi ordinare anche i documentari
   },
   {
     id: "2",
@@ -12,7 +13,7 @@ const videoData = [
     category: "youtube",
     videoSrc: "https://bysebuho.com/e/3aiaf04i095p",
     cover: "https://i.ytimg.com/vi/Vvl_qDDOTo0/hq720.jpg",
-    date: "23 Agosto 2026"
+    date: "2026-08-23" // Formato anno-mese-giorno è il top per l'ordinamento
   },
   {
     id: "3",
@@ -20,14 +21,15 @@ const videoData = [
     category: "live",
     videoSrc: "https://www.youtube.com/embed/dQw4w9WgXcQ",
     cover: "https://picsum.photos/400/225?random=3",
-    date: "15 Settembre 2026"
+    date: "2026-09-15"
   },
   {
     id: "4",
     title: "Video Fuori Categoria",
     category: "extra",
     videoSrc: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-    cover: "https://picsum.photos/400/225?random=4"
+    cover: "https://picsum.photos/400/225?random=4",
+    date: "2026-08-10"
   }
 ];
 
