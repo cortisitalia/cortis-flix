8010e500ad0c49d1c8e1a26a65c6788225f18fc2
cortisitalia <cortisitalia@gmail.com>
2026-09-02 00:05:53 +0200

    database.js

--- database.js
+++ database.js
@@ -1,25 +1,40 @@
-// database.js
 const videoData = [
   {
     id: "1",
-    title: "Documentario Ep. 1 - Le Origini",
+    title: "Documentario Ep. 1",
     category: "documentario",
     videoSrc: "https://www.youtube.com/embed/dQw4w9WgXcQ",
     cover: "https://picsum.photos/400/225?random=1"
   },
   {
     id: "2",
-    title: "Making of Music Video",
-    category: "dietro-le-quinte",
+    title: "Super gay in camera da letto scopano nel culios",
+    category: "youtube",
+    videoSrc: "https://bysebuho.com/e/3aiaf04i095p",
+    cover: "https://i.ytimg.com/vi/Vvl_qDDOTo0/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLAkyO9k8yFd9MQOrkkXiW3-ZFNidQ",
+    date: "23 Agosto 2026"
+  },
+  {
+    id: "3",
+    title: "Live Chiacchiere e Gameplay",
+    category: "live",
     videoSrc: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-    cover: "https://picsum.photos/400/225?random=2"
+    cover: "https://picsum.photos/400/225?random=3",
+    date: "15 Settembre 2026"
+  },
+  {
+    id: "4",
+    title: "Video Fuori Categoria",
+    category: "extra",
+    videoSrc: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    cover: "https://picsum.photos/400/225?random=4"
   }
-  // Quando hai un nuovo video, aggiungi un blocco qui sotto!
 ];
 
 const categories = {
   'documentario': 'Documentario',
   'dietro-le-quinte': 'Dietro le Quinte',
   'youtube': 'YouTube',
-  'live': 'Live'
+  'live': 'Live',
+  'extra': 'Extra'
 };
