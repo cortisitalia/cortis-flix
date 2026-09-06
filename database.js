c26a74d2d59c2e5833a8c6f88a1e7a3265fd79e5
cortisitalia <cortisitalia@gmail.com>
2026-09-06 23:39:01 +0200

    database.js

--- database.js
+++ database.js
@@ -38,7 +38,55 @@ const videoData = [
     videoSrc: "https://bysebuho.com/e/bqpq2o3szhvs",
     cover: "https://i.ytimg.com/vi/DOUUYxE7FeY/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDKhsCDnfjYyhbZMiEVnBDKS-1j3g",
     date: "2026-08-10"
-  }
+  },
+  {
+    id: "6",
+    title: "<PUT YOUR PHONE DOWN> IN JAPAN Day 1",
+    category: "extra",
+    videoSrc: "https://bysebuho.com/e/8k2w7jeqb8eu",
+    cover: "https://pbs.twimg.com/media/HRYIokuawAAxozX.jpg",
+    date: "2026-09-04"
+  },
+  {
+    id: "7",
+    title: "<PUT YOUR PHONE DOWN> IN JAPAN Day 2",
+    category: "extra",
+    videoSrc: "https://bysebuho.com/e/s6exkd0fv5s8",
+    cover: "https://pbs.twimg.com/media/HRcsW5BboAApmza.jpg",
+    date: "2026-09-05"
+  },
+  {
+    id: "8",
+    title: "Cucinando PACK IT UP & MONEYMONEYMONEY",
+    category: "youtube",
+    videoSrc: "https://bysebuho.com/e/he5pdfzemz8z",
+    cover: "https://i.ytimg.com/vi/kqvSN7prFjE/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDeIaqVbIXtSsM4mpXUGHEw1Ej74w",
+    date: "2026-09-02"
+  },
+  {
+    id: "9",
+    title: "ASMR di soldi che entrano",
+    category: "youtube",
+    videoSrc: "https://bysebuho.com/e/y16gdyda0jer",
+    cover: "https://i.ytimg.com/vi/4uQqmGTGjJ4/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCktK769oWcnCBnaikYcSJBAPFafg",
+    date: "2026-08-28"
+  },
+  {
+    id: "10",
+    title: "JAMES - cena",
+    category: "live",
+    videoSrc: "https://bysebuho.com/e/hcicbstmhn7z",
+    cover: "https://phinf.wevpstatic.net/MjAyNjA3MTJfNDUg/MDAxNzgzODUzMzE4MjAy.MSdCPLe4Pg2qGR5PoZasnprcPHP30CHbPeE1kIH4n24g.DET2UUK5F_fIXpT-NEaBavkkkVF_RgHy9PaQMZGnejIg.JPEG/image.jpg?type=f_or_blur_bg_640_360",
+    date: "2026-07-12"
+  },
+  {
+    id: "11",
+    title: "PACK IT UP Official Visualizer dietro le quinte",
+    category: "youtube",
+    videoSrc: "https://bysebuho.com/e/p68jmu1qtwdn",
+    cover: "https://i.ytimg.com/vi/9d39RqVQNso/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLB3eMTZcnHRtb6UrrsyscuBBGW2kw",
+    date: "2026-08-29"
+  },
 ];
 
 const categories = {
